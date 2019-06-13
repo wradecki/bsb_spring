1618c30dc8794cfdc48b8f29901702d2caa0f787
woczkowski <woczkowski@itkontekst.pl>
2019-06-13 10:25:51 +0200

    step1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,31 @@
+import org.springframework.beans.factory.groovy.GroovyBeanDefinitionReader;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.annotation.AnnotatedBeanDefinitionReader;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import pl.itkontekst.spring.CustomerService;
+
 /**
  * Created by Wojciech Oczkowski on 2019-06-13.
  */
 public class Main {
+
+    public static void main(String[] args) {
+//        ClassPathXmlApplicationContext applicationContext
+//                = new ClassPathXmlApplicationContext("/beans.xml");
+
+//        GenericApplicationContext applicationContext = new GenericApplicationContext();
+//        new XmlBeanDefinitionReader(applicationContext).loadBeanDefinitions("/beans.xml");
+//        new AnnotatedBeanDefinitionReader(applicationContext).register();
+        AnnotationConfigApplicationContext applicationContext
+                = new AnnotationConfigApplicationContext();
+        applicationContext.scan("pl.itkontekst");
+        applicationContext.refresh();
+        CustomerService customerService = applicationContext.getBean(CustomerService.class);
+
+        System.out.println("customerSevice: "+ customerService);
+
+
+    }
 }
---
+++ src/main/java/pl/itkontekst/spring/CustomerService.java
@@ -0,0 +1,17 @@
+package pl.itkontekst.spring;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Wojciech Oczkowski on 2019-06-13.
+ */
+//@Component
+//    @Repository
+    @Service
+//    @Controller
+//    @RestController
+public class CustomerService {
+}
