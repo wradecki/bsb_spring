90e0a18f761756b6a085125f8e4dc4bd93120b97
Wiktor Radecki <wiktor.radecki@mail.com>
2019-06-13 10:55:54 +0200

    bez pobierania wykona sie, poniewaz jest singleton (default)

--- src/main/java/pl/bsb/CustomerService.java
+++ src/main/java/pl/bsb/CustomerService.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 //@Component
 //@Repository
 @Service
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 //@Scope("request")
 //@Scope("webapplication")
 //@Controller
