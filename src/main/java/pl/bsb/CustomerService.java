b1678e981512251331c593bf1ee5110fef2e51c1
Wiktor Radecki <wiktor.radecki@mail.com>
2019-06-13 11:08:17 +0200

    Zmiana dokladnej klasy na proxy

--- src/main/java/pl/bsb/CustomerService.java
+++ src/main/java/pl/bsb/CustomerService.java
@@ -3,6 +3,7 @@ package pl.bsb;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
@@ -11,7 +12,7 @@ import org.springframework.stereotype.Service;
 //@Component
 //@Repository
 @Service()
-@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+@Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON, proxyMode = ScopedProxyMode.TARGET_CLASS)
 @Lazy
 //@Scope("request")
 //@Scope("webapplication")
