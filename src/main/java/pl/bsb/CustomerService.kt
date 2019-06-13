97cc77d124ab8e791a55dee2a7287e1279ce4b15
Wiktor Radecki <wiktor.radecki@mail.com>
2019-06-13 10:48:07 +0200

    Dla webowych kontenerow

--- src/main/java/pl/bsb/CustomerService.kt
+++ src/main/java/pl/bsb/CustomerService.kt
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Service;
 //@Repository
 @Service
 @Scope("prototype")
+//@Scope("request")
+//@Scope("webapplication")
 //@Controller
 //@RestController
 public class CustomerService {
