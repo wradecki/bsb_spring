2706029b197c4540bfa72179d345b8f0851b78fc
wradecki <wiktor.radecki@mail.com>
2019-06-14 09:17:19 +0200

    Lub przez adnotacji bean

--- src/main/java/pl/bsb/Config.java
+++ src/main/java/pl/bsb/Config.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class Config {
 
-    @Bean(name = "custService")
+    @Bean(name = "custService", initMethod = "test")
     public CustomerService getCustomerService(){
         return new CustomerService();
     }
