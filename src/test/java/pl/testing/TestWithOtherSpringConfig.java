454b183145cf2cd6396f3084ac3f9aed96271991
wradecki <wiktor.radecki@mail.com>
2019-06-14 11:15:45 +0200

    teraz trybi

--- src/test/java/pl/testing/TestWithOtherSpringConfig.java
+++ src/test/java/pl/testing/TestWithOtherSpringConfig.java
@@ -2,15 +2,19 @@ package pl.testing;
 
 import org.junit.Assert;
 import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.rules.SpringClassRule;
+import org.springframework.test.context.junit4.rules.SpringMethodRule;
 
 @ContextConfiguration(classes = ConfigTesting.class)
 public class TestWithOtherSpringConfig {
     @ClassRule
     public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
+    @Rule
+    public final SpringMethodRule SPRING_METHOD_RULE = new SpringMethodRule();
 
     @Autowired
     TestingService testingService;
